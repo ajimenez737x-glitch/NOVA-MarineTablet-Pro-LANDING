31d84e6305bdcfdf21722ee77118e71476c5fec7
ajimenez737x-glitch <ajimenez737x@gmail.com>
2026-06-23 11:46:45 +0200

    Remove .vercel build cache and add to .gitignore

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CgmpRSu3.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/aleji/Documents/PROYECTOS WEB/PRODUCTOS/MODELO PRODUCTOS/WEB MODELO REFORMAS/reformas-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-D2lPEkhu.js"] }, "/": { filePath: "C:/Users/aleji/Documents/PROYECTOS WEB/PRODUCTOS/MODELO PRODUCTOS/WEB MODELO REFORMAS/reformas-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-kAAe2VAG.js"] } }, clientEntry: "/assets/index-D2lPEkhu.js" });
-export {
-  tsrStartManifest
-};
